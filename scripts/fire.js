0d67402143bc6aff48e9dc8aae0c2d93de7367c5
Acero Liang <aceroliang@Aceros-MacBook-Air.local>
2023-11-05 01:56:55 -0400

    landing page prelim styles

--- scripts/fire.js
+++ scripts/fire.js
@@ -1,9 +1,9 @@
 /** Fire: The fire model specific code goes here. */
-async function GameLoop() {
-    CallCommand("go")
-    var Fires = await RunReporter("count fires");
-    if (Fires == 0) {
-        // The game ends
+// async function GameLoop() {
+//     CallCommand("go")
+//     var Fires = await RunReporter("count fires");
+//     if (Fires == 0) {
+//         // The game ends
         
-    } else return await GameLoop();
-}
+//     } else return await GameLoop();
+// }
