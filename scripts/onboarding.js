1391afc428a5afe72be6725a047558574715d069
Acero Liang <aceroliang@Aceros-MacBook-Air.local>
2023-11-05 02:52:52 -0500

    minor onboarding changes

--- scripts/onboarding.js
+++ scripts/onboarding.js
@@ -1,5 +1,20 @@
 // Page data
-const pages = [
+const pages = [[{
+    img: 'assets/wolfsheep.png',
+    title: 'WHAT IS IT?',
+    description: 'This model explores the stability of predator-prey ecosystems. Such a system is called unstable if it tends to result in extinction for one or more species involved. In contrast, a system is stable if it tends to maintain itself over time, despite fluctuations in population sizes.'
+},
+{
+    img: 'assets/sheep.gif',
+    title: 'HOW DOES IT WORK?',
+    description: "There are two main variations to this model. In the first variation, the \"sheep-wolves\" version, wolves and sheep wander randomly around the landscape, while the wolves look for sheep to prey on. Each step costs the wolves energy, and they must eat sheep in order to replenish their energy - when they run out of energy they die. To allow the population to continue, each wolf or sheep has a fixed probability of reproducing at each time step. In this variation, we model the grass as \"infinite\" so that sheep always have enough to eat, and we don't explicitly model the eating or growing of grass. As such, sheep don't either gain or lose energy by eating or moving."
+},
+{
+    img: 'assets/sheep2.gif',
+    title: 'HOW DOES IT WORK?',
+    description: 'The second variation, the "sheep-wolves-grass" version explictly models grass (green) in addition to wolves and sheep. The behavior of the wolves is identical to the first variation, however this time the sheep must eat grass in order to maintain their energy - when they run out of energy they die. Once grass is eaten it will only regrow after a fixed amount of time. ',
+    buttonText: 'GET STARTED'
+}],[
     {
         img: 'assets/fire.png',
         title: 'WHAT IS IT?',
@@ -16,11 +31,11 @@ const pages = [
         description: 'The fire spreads in four directions: north, east, south, and west.',
         buttonText: 'GET STARTED'
     }
-];
+], null];
 
 
-function updateContent(pageIndex) {
-    const { img, title, description, buttonText } = pages[pageIndex];
+function updateContent(pageIndex, modelIndex) {
+    const { img, title, description, buttonText } = pages[modelIndex][pageIndex];
     document.querySelector('.demo-img').src = img;
     document.querySelector('.onboarding-title').innerText = title;
     document.querySelector('.onboarding-descript').innerText = description;
@@ -30,22 +45,28 @@ function updateContent(pageIndex) {
     }
 }
 
-function loadFireModel(parent) {
-    parent.innerHTML = `<iframe id="fire-model" src="simulation.html#models/fire.nlogo"></iframe>`;
-}
+// function loadFireModel(parent) {
+//     parent.innerHTML = `<iframe id="fire-model" src="simulation.html#models/fire.nlogo"></iframe>`;
+// }
 
-document.querySelector('.container').addEventListener('click', function(event) {
-    const target = event.target;
 
-    if (target.matches('#button-next')) {
-        currentPage++;
-        if (currentPage < pages.length) {
-            updateContent(currentPage);
-        } else {
-            loadFireModel(document.querySelector('.container'));
+function loadPageContent(currentPage, modelIndex) {
+    document.querySelector('.container').addEventListener('click', function(event) {
+        const target = event.target;
+    
+        if (target.matches('#button-next')) {
+            currentPage++;
+            if (currentPage < pages.length) {
+                updateContent(currentPage, modelIndex);
+            } else {
+                //loadFireModel(document.querySelector('.container'));
+                // clear intro, show the model 
+                document.querySelector('#intro').remove();
+                document.querySelector('.model').classList.remove('invisible-element');
+            }
         }
-    }
-
-});
+    });
+    updateContent(currentPage, modelIndex);
+}
 
 
