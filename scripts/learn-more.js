557f57ac1303b39e20e89b5dfecacfe0c338a6b1
Acero Liang <aceroliang@aceros-air.mynetworksettings.com>
2023-11-26 21:44:25 -0500

    support

--- scripts/learn-more.js
+++ scripts/learn-more.js
@@ -1,9 +1,16 @@
 /* Add event listeners to the visit site buttons */
 
 const visitSiteButtons = document.querySelectorAll('.visit-button');
+const supportButtons = document.querySelectorAll('.support-button');
 
 visitSiteButtons.forEach((button) => {
     button.addEventListener('click' , () => {
         top.location.href = 'https://www.netlogoweb.org/';
+    });
+});
+
+supportButtons.forEach((button) => {
+    button.addEventListener('click' , () => {
+        window.location.href 
     });
 });
