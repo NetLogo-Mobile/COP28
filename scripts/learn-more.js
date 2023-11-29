0e1ca4d70ad46e0b30dbd0dfa610c5eb491a3cda
John Chen <civitas@u.northwestern.edu>
2023-11-29 16:13:08 -0600

    More GA

--- scripts/learn-more.js
+++ scripts/learn-more.js
@@ -1,35 +1,28 @@
-/* Add event listeners to the visit site buttons */
-
-const visitSiteButtons = document.querySelectorAll('.visit-button');
-const supportButtons = document.querySelectorAll('.support-button');
-
-
-
-visitSiteButtons.forEach((button) => {
-    button.addEventListener('click' , () => {
-        top.location.href = 'https://www.netlogoweb.org/';
-    });
-});
-
-supportButtons.forEach((button) => {
-    button.addEventListener('click' , () => {
-
-    });
-});
-
 // about netlogo btns 
 const downloadBtns = document.querySelectorAll('.download-btn');
-    if(downloadBtns.length > 0){
-        downloadBtns[0].addEventListener('click' , () => {
+if (downloadBtns.length > 0) {
+    downloadBtns[0].addEventListener('click', () => {
+        // Record the event
+        gtag("event", "select_content", {
+            content_type: "netlogo-desktop"
+        });
         top.location.href = 'https://ccl.northwestern.edu/netlogo/download.shtml';
         console.log("download");
     });
-    
-    downloadBtns[1].addEventListener('click' , () => {
+
+    downloadBtns[1].addEventListener('click', () => {
+        // Record the event
+        gtag("event", "select_content", {
+            content_type: "netlogo-web"
+        });
         top.location.href = 'https://www.netlogoweb.org/launch';
     });
-    
-    downloadBtns[2].addEventListener('click' , () => {
+
+    downloadBtns[2].addEventListener('click', () => {
+        // Record the event
+        gtag("event", "select_content", {
+            content_type: "turtle-universe"
+        });
         top.location.href = 'https://turtlesim.com/products/turtle-universe/';
     });
 }
@@ -37,14 +30,31 @@ const downloadBtns = document.querySelectorAll('.download-btn');
 // subscribe btn
 function subscribeToList() {
     let email = document.querySelector('#subscribe-input').value;
-    console.log(email);
-    // do smth
+    // Validate email
+    if (!isValidEmail(email)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+    // Record the event
+    gtag("event", "select_content", {
+        content_type: "subscription"
+    });
+}
+
+// email validation
+function isValidEmail(email) {
+    var regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    return regex.test(email);
 }
 
 //donate btn
 const donateBtn = document.querySelector('.donate-btn');
-if(donateBtn){
-    donateBtn.addEventListener('click' , () => {
+if (donateBtn) {
+    donateBtn.addEventListener('click', () => {
+        // Record the event
+        gtag("event", "select_content", {
+            content_type: "donation"
+        });
         top.location.href = 'https://secure.ard.northwestern.edu/s/1479/282-giving/basic-page-nonav-campaign.aspx?sid=1479&gid=282&pgid=19841&cid=31575';
     });
 }
